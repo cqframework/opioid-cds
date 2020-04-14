039964810187eac2f074d1fe3e22b4155f2f8ce0
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-14 16:08:17 -0600

    Updated genonce script; ignore build files.

--- _genonce.sh
+++ _genonce.sh
@@ -17,13 +17,13 @@ echo "$txoption"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	JAVA -jar $publisher -ig ig.ini $txoption $*
+	JAVA -jar $publisher -ig ig.json $txoption $*
 
 else
 	publisher=../$publisher_jar
 	echo $publisher
 	if test -f "$publisher"; then
-		JAVA -jar $publisher -ig ig.ini $txoption $*
+		JAVA -jar $publisher -ig ig.json $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
