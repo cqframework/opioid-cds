33e00cf74471454483c949a48baf02fdaa4e5d74
Adam Stevenson <stevenson_adam@yahoo.com>
2020-05-11 13:42:14 -0600

    Updated refresh scripts to include igrp; Refresh libraries - relatedArtifacts updates.

--- _refresh.bat
+++ _refresh.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
-SET tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
+SET tooling_jar=tooling-1.0.2-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
+SET resources_path=%~dp0/input/resources
+SET ig_resource_path=%~dp0/input/opioid-cds.xml
 
 ECHO Checking internet connection...
 PING tx.fhir.org -n 1 -w 1000 | FINDSTR TTL && GOTO isonline
@@ -17,11 +19,11 @@ SET fsoption=-fs http://cds-sandbox.alphora.com/cqf-ruler-dstu3/fhir/
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir3 -t -d -p %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir3 -t -d -p %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir3 -t -d -p %fsoption%
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir3 -t -d -p %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir3 -t -d -p %fsoption%
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir3 -t -d -p %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir3 -t -d -p %fsoption%
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir3 -t -d -p %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
--- _refresh.sh
+++ _refresh.sh
@@ -2,6 +2,9 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
+resources_path=$PWD/input/resources
+ig_resource_path=./input/opioid-cds.xml
+
 set -e
 echo Checking internet connection...
 wget -q --spider tx.fhir.org
@@ -18,13 +21,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -iv=fhir3 -t -d -p $fsoption
-
+	JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -iv=fhir3 -t -d -p $fsoption
+		JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
