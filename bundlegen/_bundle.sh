0e0276266d397ed8c099ccb8d6aa2724c9190832
Adam Stevenson <stevenson_adam@yahoo.com>
2020-06-29 19:21:13 -0600

    Added refreshQuickStart script.

--- bundlegen/_bundle.sh
+++ bundlegen/_bundle.sh
@@ -3,7 +3,7 @@
 tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 bundlegen_path=$PWD/bundlegen
-
+sourcefiles_path=$bundlegen_path/sourcefiles
 
 set -e
 echo Checking internet connection...
@@ -22,12 +22,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -BundleResources -ptd=$bundlegen_path op=$bundlegen_path -v=stu3 -e=json
+	JAVA -jar $tooling -BundleResources -ptd=$sourcefiles_path op=$bundlegen_path -v=stu3 -e=json
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -BundleResources -ptd=$bundlegen_path op=$bundlegen_path -v=stu3 -e=json
+		JAVA -jar $tooling -BundleResources -ptd=$sourcefiles_path op=$bundlegen_path -v=stu3 -e=json
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
