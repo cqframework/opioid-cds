e3b1f95fea751ed45d1e1a3d8f98f9361805c0da
Adam Stevenson <stevenson_adam@yahoo.com>
2020-12-21 20:45:48 -0700

    Lift for 1.3.1 tooling; Disable ProcedureRequest and MedicationStatement retrieves; Various fixes.

--- _refresh.sh
+++ _refresh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.2.0-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 resources_path=$PWD/input/resources
-ig_resource_path=./input/opioid-cds.xml
+# ig_resource_path=./input/opioid-cds.xml
+ig_ini_path=$PWD/ig.ini
 
 set -e
 echo Checking internet connection...
@@ -21,18 +22,19 @@ fi
 echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
+
 if test -f "$tooling"; then
-	#JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
+	#JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -cdsig -t -d -p $fsoption
+	JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -rp="$resources_path" -cdsig -t -d -p $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		#JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
-		JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir3 -t -d -p $fsoption
+		#JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -cdsig -t -d -p $fsoption
+		JAVA -jar $tooling -RefreshIG -ini="$ig_ini_path" -rp="$resources_path"  -cdsig -t -d -p $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
 fi
 
-sh input/pagecontent/quick-start-bundles/_refreshQuickStart.sh
+#sh input/pagecontent/quick-start-bundles/_refreshQuickStart.sh
